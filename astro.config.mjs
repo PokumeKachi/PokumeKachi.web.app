ada7d7a3cd97bf6f6d33c89f00e39a05fe080888
PokumeKachi <108186213+PokumeKachi@users.noreply.github.com>
2026-09-08 09:55:28 +0700

    feat: move entire project to legacy

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,16 @@
 // @ts-check
 import { defineConfig } from "astro/config";
 import tailwindcss from "@tailwindcss/vite";
+import { fileURLToPath, URL } from "node:url";
 
 // https://astro.build/config
 export default defineConfig({
-    vite: { plugins: [tailwindcss()] },
+    vite: {
+        plugins: [tailwindcss()],
+        resolve: {
+            alias: {
+                "@": fileURLToPath(new URL("./src", import.meta.url)),
+            },
+        },
+    },
 });
